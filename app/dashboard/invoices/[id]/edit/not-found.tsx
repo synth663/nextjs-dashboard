372cb24e5fd2fe86b7e10f96cdde3a9d64a59abf
synth663 <mittalhitansh41@gmail.com>
2026-04-30 02:32:34 +0530

    Completed Dashboard Tutorial using App Router

---
+++ app/dashboard/invoices/[id]/edit/not-found.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import { FaceFrownIcon } from '@heroicons/react/24/outline';
+
+export default function NotFound(){
+    return (
+        <main className='flex h-full flex-col items-center justify-center gap-2'>
+            <FaceFrownIcon className='w-10 text-grey-400' />
+            <h2 className='text-xl font-semibold'>404 NOT FOUND</h2>
+            <p>Could Not Find the Requested Invoice.</p>
+            <Link
+                href={'/dashboard/invoices'}
+                className='mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400'
+            >
+                Go Back
+            </Link>
+        </main>
+    );
+} 
